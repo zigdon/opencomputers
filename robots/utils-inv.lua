d46c21e1ce1c08c8b12d49aada99cd6748d5a5d0
Dan Boger <dan@peeron.com>
2020-11-07 09:00:43 -0800

    - stockUp tries to store the wrong items before tossing them

--- robots/utils-inv.lua
+++ robots/utils-inv.lua
@@ -1,4 +1,5 @@
 local inv = require("component").inventory_controller
+local sides = require("sides")
 local r = require("robot")
 local s = require("serialization").serialize
 
@@ -37,7 +38,9 @@ function findExternalItem(dir, name, damage)
 end
 
 function stockUp(dir, targetLevels)
+  assert(type(dir) == "string")
   assert(type(targetLevels) == "table")
+  local dirNum = sides[dir]
   for _, t in pairs(targetLevels) do
     assert(t.name ~= nil)
     if t.count == nil then
@@ -51,15 +54,15 @@ function stockUp(dir, targetLevels)
     local item = inv.getStackInInternalSlot(t.slot)
     if item ~= nil then
       if item.name ~= t.name or t.damage ~= nil and item.damage ~= t.damage then
-         r.dropDown()
+         r.drop() or r.dropDown()
       else
         need = t.count - item.size
       end
     end
     if need > 0 then
-      local remoteSlot = findExternalItem(dir, t.name, t.damage)
+      local remoteSlot = findExternalItem(dirNum, t.name, t.damage)
       assert(remoteSlot ~= nil)
-      inv.suckFromSlot(dir, remoteSlot, need)
+      inv.suckFromSlot(dirNum, remoteSlot, need)
       assert(inv.getStackInInternalSlot(t.slot).size >= t.count)
     end
   end
